9847d674e295f10d327a2d546304e6133009f65c
Cyril Feraudet <cyril@feraudet.com>
2019-09-30 13:22:09 +0200

    Add source code

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "aws_cloudwatch_event_rule" "this" {
+  name                = "lambda_warmer-${var.function_name}"
+  description         = "Keep warm ${var.function_name} lambda"
+  schedule_expression = var.rate
+}
+
+resource "aws_cloudwatch_event_target" "this" {
+  rule      = aws_cloudwatch_event_rule.this.name
+  target_id = var.function_name
+  arn       = var.function_arn
+  input_transformer {
+    input_template = var.input
+  }
+}
+
+resource "aws_lambda_permission" "this" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = var.function_arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.this.arn
+}
+
