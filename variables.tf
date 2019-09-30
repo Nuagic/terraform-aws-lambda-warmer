ee33ac45a4361ac7ae95d09ff695e45640372feb
Cyril Feraudet <cyril@feraudet.com>
2019-09-30 13:23:07 +0200

    Fix typo

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "function_name" {}
 variable "function_arn" {}
-variable "input" { default="{\"Records\":[]}" }
-variable "rate" { default="rate(4 minutes)" }
+variable "input" { default = "{\"Records\":[]}" }
+variable "rate" { default = "rate(4 minutes)" }
